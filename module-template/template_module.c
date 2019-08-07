0d8c02dd06082b2328f54e9678fade0c159bba1b
Roman Khomenko <romankhomenko1995@gmail.com>
2019-08-07 23:08:18 +0300

    Изменен уровень логирования

--- module-template/template_module.c
+++ module-template/template_module.c
@@ -8,13 +8,13 @@ MODULE_AUTHOR("Roman Khomenko <romankhomenko1995@gmail.com>");
 
 static int __init module_init_proc(void)
 {
-	pr_debug("Template module start!\n");
+	pr_info("Template module start!\n");
 	return 0;
 }
 
 static void __exit module_exit_proc(void)
 {
-	pr_debug("Template module exited!\n");
+	pr_info("Template module exited!\n");
 }
 
 module_init(module_init_proc);
