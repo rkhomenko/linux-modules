fa166b93b8161c56ac65784c8b70e8a64948c56a
Roman Khomenko <romankhomenko1995@gmail.com>
2019-08-07 22:42:09 +0300

    Код исправлен с учетом стандартов форматирования ядра Linux

--- module-template/template_module.c
+++ module-template/template_module.c
@@ -1,16 +1,20 @@
+// SPDX-License-Identifier: GPL-2.0-or-later
+
 #include <linux/init.h>
 #include <linux/module.h>
 
 MODULE_LICENSE("GPL");
 MODULE_AUTHOR("Roman Khomenko <romankhomenko1995@gmail.com>");
 
-static int __init module_init_proc(void) {
-    printk("Template module start!\n");
-    return 0;
+static int __init module_init_proc(void)
+{
+	pr_debug("Template module start!\n");
+	return 0;
 }
 
-static void __exit module_exit_proc(void) {
-    printk("Template module exited!\n");
+static void __exit module_exit_proc(void)
+{
+	pr_debug("Template module exited!\n");
 }
 
 module_init(module_init_proc);
